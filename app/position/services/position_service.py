a425bd9bdc95a339bfe1b0fff669ca48639a1c11
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-03 04:10:48 -0500

    feat: proceso de asignación de candidato como elegido en posición, y pruebas unitarias

--- app/position/services/position_service.py
+++ app/position/services/position_service.py
@@ -1,6 +1,11 @@
+from fastapi import HTTPException
 from typing import List, TYPE_CHECKING
 
-from app.database.schemas import CandidateResponse
+from app.database.schemas import (
+    CandidateResponse,
+    PositionUpdate,
+    PositionUpdateResponse,
+)
 
 if TYPE_CHECKING:
     from app.position.repositories.position_repository import PositionRepository
@@ -12,7 +17,7 @@ class PositionService:
 
     async def get_positions(self):
         results = await self.position_repository.get_open_positions_with_details()
-
+        print(results)
         open_positions_with_details = []
         for open_position, project, soft_skill_ids, technology_ids in results:
             open_positions_with_details.append(
@@ -43,3 +48,39 @@ class PositionService:
             )
 
         return candidates_output
+
+    async def update_position_chosen_candidate(
+        self, position_id: int, position_data: PositionUpdate
+    ) -> PositionUpdateResponse:
+        candidates = await self.position_repository.get_open_position_candidates(
+            position_id
+        )
+        not_candidate_for_pos = True
+        for candidate in candidates:
+            if candidate.candidate_id == position_data.candidate_id:
+                not_candidate_for_pos = False
+        if not_candidate_for_pos:
+            raise HTTPException(
+                412,
+                "Candidato no hace parte de proceso de selección de posición indicada",
+            )
+        updated_position = await self.position_repository.update_open_position(
+            position_id, position_data.candidate_id
+        )
+        positions = await self.position_repository.get_project_positions(
+            updated_position.project_id
+        )
+        is_project_closed = True
+        for position in positions:
+            if position.is_open:
+                is_project_closed = False
+        if is_project_closed:
+            await self.position_repository.close_project(updated_position.project_id)
+        return PositionUpdateResponse.model_validate(
+            {
+                "id": updated_position.id,
+                "project_id": updated_position.project_id,
+                "is_open": updated_position.is_open,
+                "candidate_id": updated_position.candidate_id,
+            }
+        )
--- tests/positions/services/test_positions_service.py
+++ tests/positions/services/test_positions_service.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi import HTTPException
 from unittest.mock import Mock, AsyncMock
 
 from app.position.services.position_service import PositionService
@@ -9,6 +10,14 @@ class TestPositionService:
     async def test_position_service(self):
         mocked_repository = Mock()
         mocked_repository.get_open_positions_with_details = AsyncMock()
+        mocked_repository.get_open_positions_with_details.return_value = [
+            Mock(
+                open_position=Mock(id=1, position_name="Test position A", candidate_id=1, project_id=1, is_open=False),
+                project=Mock(customer_id=1, name="Project name", is_team_complete=False, id=1, description="A description"),
+                soft_skill_ids=[1, 2],
+                technology_ids=[3, 4],
+            )
+        ]
 
         service = PositionService(mocked_repository)
 
@@ -43,3 +52,107 @@ class TestPositionService:
         assert func_response[0]["technical_score"] == 1
         assert func_response[0]["softskill_score"] == 2
         assert func_response[0]["general_score"] == 3
+
+    @pytest.mark.asyncio
+    async def test_update_position_chosen_candidate(self):
+        mocked_repository = Mock()
+        mocked_repository.get_open_position_candidates = AsyncMock()
+        mocked_repository.update_open_position = AsyncMock()
+        mocked_repository.get_project_positions = AsyncMock()
+        mocked_repository.close_project = AsyncMock()
+
+        position_id = 2
+        candidate_id = 3
+        project_id = 1
+
+        position_data = Mock(candidate_id=candidate_id)
+
+        base_candidates = [
+            Mock(candidate_id=3, technical_score=0, softskill_score=0, general_score=0),
+        ]
+        mocked_repository.get_open_position_candidates.return_value = base_candidates
+
+        base_position = Mock(
+            id=position_id,
+            project_id=project_id,
+            is_open=False,
+            candidate_id=candidate_id,
+        )
+        mocked_repository.update_open_position.return_value = base_position
+
+        mocked_repository.get_project_positions.return_value = [base_position]
+
+        service = PositionService(mocked_repository)
+
+        func_response = await service.update_position_chosen_candidate(
+            position_id, position_data
+        )
+
+        assert func_response.id == position_id
+        assert func_response.project_id == project_id
+        assert func_response.is_open == False
+        assert func_response.candidate_id == candidate_id
+
+    @pytest.mark.asyncio
+    async def test_update_position_candidate_was_not_preselected(self):
+        mocked_repository = Mock()
+        mocked_repository.get_open_position_candidates = AsyncMock()
+
+        position_id = 2
+        candidate_id = 3
+
+        position_data = Mock(candidate_id=candidate_id)
+
+        base_candidates = []
+        mocked_repository.get_open_position_candidates.return_value = base_candidates
+
+        service = PositionService(mocked_repository)
+
+        with pytest.raises(HTTPException):
+            await service.update_position_chosen_candidate(position_id, position_data)
+
+    @pytest.mark.asyncio
+    async def test_update_position_chosen_candidate_project_still_open(self):
+        mocked_repository = Mock()
+        mocked_repository.get_open_position_candidates = AsyncMock()
+        mocked_repository.update_open_position = AsyncMock()
+        mocked_repository.get_project_positions = AsyncMock()
+        mocked_repository.close_project = AsyncMock()
+
+        position_id = 3
+        candidate_id = 4
+        project_id = 2
+
+        position_data = Mock(candidate_id=candidate_id)
+
+        base_candidates = [
+            Mock(
+                candidate_id=candidate_id,
+                technical_score=0,
+                softskill_score=0,
+                general_score=1,
+            ),
+        ]
+        mocked_repository.get_open_position_candidates.return_value = base_candidates
+
+        base_position = Mock(
+            id=position_id,
+            project_id=project_id,
+            is_open=False,
+            candidate_id=candidate_id,
+        )
+        mocked_repository.update_open_position.return_value = base_position
+
+        base_position_2 = Mock(
+            id=position_id + 1, project_id=project_id, is_open=True, candidate_id=None
+        )
+        mocked_repository.get_project_positions.return_value = [
+            base_position,
+            base_position_2,
+        ]
+
+        service = PositionService(mocked_repository)
+
+        await service.update_position_chosen_candidate(position_id, position_data)
+
+        assert mocked_repository.update_position_chosen_candidate.call_count == 0
